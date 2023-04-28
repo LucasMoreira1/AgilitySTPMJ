e6248df9d453624b5e658dbc2494a5faa65edca5
Lucas Moreira <lucas_moreira@outlook.com>
2023-04-28 10:48:41 -0300

    campo valor lançamento

--- FormLancamentos.Designer.cs
+++ FormLancamentos.Designer.cs
@@ -196,7 +196,6 @@
             // 
             // txtValor
             // 
-            this.txtValor.Enabled = false;
             this.txtValor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtValor.Location = new System.Drawing.Point(607, 339);
             this.txtValor.Name = "txtValor";
