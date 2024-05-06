efaacf5795b886c4d0ed383847166411020f6997
Lucas Moreira <lucas_moreira@outlook.com>
2024-05-06 16:30:49 -0300

    ajuste na conexão

--- CRUD.cs
+++ CRUD.cs
@@ -9,8 +9,11 @@ namespace Programa_STPMJ
         private static string getConnectionString()
         {
             //string conString = "Server=108.167.132.199;Database=soluc963_STPMJ;Uid=soluc963_STPMJ;Pwd=R~X4ro$%K18b; ";
-            string conString = "Server=187.95.26.193;Port=3306;Database=soluc963_STPMJ;Uid=Agility;Pwd=!Parrudo292701;";
-            return conString;
+            //string conString = "Server=187.95.26.193;Port=3306;Database=soluc963_STPMJ;Uid=Agility;Pwd=!Parrudo292701;";
+            //string conString = "Server=187.95.26.142;Port=3306;Database=soluc963_STPMJ;Uid=Agility;Pwd=!Parrudo292701;";
+            //string conString = "Server=187.95.25.106;Port=3306;Database=soluc963_STPMJ;Uid=Agility;Pwd=!Parrudo292701;";
+            string conString = "Server=mysql-agility.advogadodigital.click;Port=3306;Database=soluc963_STPMJ;Uid=Agility;Pwd=!Parrudo292701;";
+            return conString; 
         }
 
         public static MySqlConnection con = new MySqlConnection(getConnectionString());
